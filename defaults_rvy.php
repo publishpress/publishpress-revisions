271877d8ec8f8dae04ade482958208a13d4da774
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2025-08-28 14:58:24 -0400

    Update defaults_rvy.php

--- defaults_rvy.php
+++ defaults_rvy.php
@@ -70,6 +70,7 @@ function rvy_default_options_sitewide() {
 		'delete_settings_on_uninstall' => true,
 		'apply_post_exceptions' => true,
 		'legacy_notifications' => true,
+		'approve_button_verbose' => true,
 	);
 
 	if ( $other_options = array_diff_key( rvy_default_options(), $def ) ) {
@@ -139,6 +140,7 @@ function rvy_default_options() {
 		'delete_settings_on_uninstall' => 0,
 		'apply_post_exceptions' => 0,
 		'legacy_notifications' => 0,
+		'approve_button_verbose' => 0,
 	);
 
 	return $def;
