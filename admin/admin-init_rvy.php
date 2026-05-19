e763c413c1de07f856c5fede7c5a32dff2ea69b8
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2026-05-19 15:50:23 -0400

    Add nonce for refresh_updates

--- admin/admin-init_rvy.php
+++ admin/admin-init_rvy.php
@@ -457,7 +457,7 @@ function rvy_admin_init() {
 		include_once(REVISIONARY_PRO_ABSPATH . '/includes-pro/pro-activation-ajax.php');
 	}
 
-	if (defined('PUBLISHPRESS_REVISIONS_PRO_VERSION') && !empty($_REQUEST['rvy_refresh_updates'])) {
+	if (defined('PUBLISHPRESS_REVISIONS_PRO_VERSION') && !empty($_REQUEST['rvy_refresh_updates']) && check_admin_referer('rvy_refresh_updates') && current_user_can('update_plugins')) {
 		do_action('revisionary_refresh_updates');
 	}
 
