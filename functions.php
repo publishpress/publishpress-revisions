0ef9703504ea45d07d2f46e2464131b75376c7d3
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2026-06-04 13:33:30 -0400

    Correct existing scheduled revisions with wrong post_status

--- functions.php
+++ functions.php
@@ -600,9 +600,12 @@ function pp_revisions_plugin_updated($current_version, $args = []) {
     }
 
     if (version_compare($last_ver, '3.8.2', '<')) {
-        if (get_option('rvy_permissions_compat_mode')) {
-            $wpdb->query("UPDATE $wpdb->posts SET post_status = 'future-revision' WHERE post_mime_type = 'future-revision'");
-        }
+        $wpdb->query(
+            $wpdb->prepare(
+                "UPDATE $wpdb->posts SET post_status = %s WHERE post_mime_type = 'future-revision'", 
+                (get_option('rvy_permissions_compat_mode')) ? 'future-revision' : 'pending'
+            )
+        );
     }
 
     if (version_compare($last_ver, '3.7.20', '<')) {
